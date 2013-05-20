891a0df78d1e5c6535a9477adc90dc531785aa02
unknown <sashida@SBD751SASHIDA1.nature.local>
2013-05-20 18:43:08 +0900

    initial commit

---
+++ src/main/java/com/corejsf/LocaleChanger.java
@@ -0,0 +1,29 @@
+package com.corejsf;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class LocaleChanger implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String languageCode;
+	
+	public String changeLocale() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getViewRoot().setLocale(new Locale(languageCode));
+		
+		return null;
+	}
+
+	public void setLanguageCode(String languageCode) {
+		this.languageCode = languageCode;
+	}
+}
